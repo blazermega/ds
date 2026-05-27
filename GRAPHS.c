6a10ee0d9f2086c19531928260d06a83a6fa01d9
Varun <saivarun0107@gmail.com>
2026-05-27 11:04:44 +0530

    `1`

--- GRAPHS.c
+++ GRAPHS.c
@@ -1,2 +1,50 @@
-#include<stdio.h
+#include<stdio.h>
 #include<stdlib.h>
+
+struct graph
+{
+    int data;
+    int bfs,dfs;
+    struct graph *next;
+};
+typedef struct graph graph;
+
+/*struct list
+{
+    graph data;
+    struct list *next;
+};*/
+//typedef struct list list;
+int exist(graph **list,int data);
+
+int main()
+{
+    int n;
+    printf("Enter the number of nodes\n");
+    scanf("%d",&n);
+    graph** list = (graph**)malloc(sizeof(graph*)*n);
+    for(int i=0;i<n;i++)
+    {
+        list[i] = (graph*)malloc(sizeof(graph));
+        printf("Enter the data of node %d\n", i+1);
+        scanf("%d",&list[i]->data);
+        list[i]->next = NULL;
+        list[i]->bfs = 0;
+        list[i]->dfs = 0;
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        char op;
+        printf("enter adjecent nodes of %d", list[i]->data);
+        do
+        {
+            int data;
+            printf("enter the node\n");
+            scanf("%d",&data);
+            if (exist(list,data))
+
+        }
+    }
+
+}
